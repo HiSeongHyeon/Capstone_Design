40586814f317409e4a7efa542d6e215b72d533cf
hyobon99 <rngyqhs@uos.ac.kr>
2025-11-03 18:18:37 +0900

    modify binary func to grayscale image

--- compute_volume/tools/calibration.py
+++ compute_volume/tools/calibration.py
@@ -23,18 +23,34 @@ class Calibration:
         print(f"Normalized data saved to {save_path}")
     
     #임계값 이하는 검은색, 이상은 흰색으로 변환
-    def convert2binary(self, threshold):
+    # def convert2binary(self, threshold):
+    #     norm_data = self.normalize()
+    #     binary_data = np.zeros_like(norm_data, dtype=np.uint8)
+    #     binary_data[norm_data >= threshold] = 255
+    #     # #비최대 억제 적용으로 노이즈 감소
+    #     # kernel = np.ones((3,3), np.uint8)
+    #     # binary_data = cv.dilate(binary_data, kernel, iterations=1)
+    #     #.jpg 이미지 데이터로 변환
+    #     cv.imwrite('cali_02.jpg', binary_data)
+    #     print("Binary data saved to binary_data.jpg")
+    #     return binary_data
+    #임계하한, 상한 범위의 데이터를 0~255 그레이스케일 이미지로 변환
+    def convert2grayscale(self, lower_bound, upper_bound):
         norm_data = self.normalize()
-        binary_data = np.zeros_like(norm_data, dtype=np.uint8)
-        binary_data[norm_data >= threshold] = 255
+        gray_data = np.zeros_like(norm_data, dtype=np.uint8)
+        #하한과 상한 사이의 값들을 0~255로 매핑
+        mask = (norm_data >= lower_bound) & (norm_data <= upper_bound)
+        gray_data[mask] = ((norm_data[mask] - lower_bound) * 255 / (upper_bound - lower_bound)).astype(np.uint8)
+        #가우시안 블러링
+        gray_data = cv.GaussianBlur(gray_data, (5, 5), 0)
         #.jpg 이미지 데이터로 변환
-        cv.imwrite('cali_08.jpg', binary_data)
-        print("Binary data saved to binary_data.jpg")
-        return binary_data
+        cv.imwrite('grayscale_data.jpg', gray_data)
+        print("Grayscale data saved to grayscale_data.jpg")
+        return gray_data
     def can_calib(self, file_path):
         #이미지를 받아 캘리브레이션 패턴을 찾는지 확인해주는 함수
         image = cv.imread(file_path, cv.IMREAD_GRAYSCALE)
-        pattern_size = (4, 7)  # 체스보드 패턴의 내부 코너 수
+        pattern_size = (7, 7)  # 체스보드 패턴의 내부 코너 수
         ret, corners = cv.findChessboardCorners(image, pattern_size, None)
         #캘리브레이션 패턴이 발견되었는지 여부와 코너 좌표 반환
         if ret:
@@ -47,10 +63,11 @@ class Calibration:
         return ret, corners
 
 if __name__ == "__main__":
-    calib = Calibration('./example/smoothed_cali_08.npy')
+    calib = Calibration('./example/1103/calibration_04.npy')
     # calib.save_normalized('./example/normalized_cali_08.npy')
     # calib.convert2binary(328)
-    for i in range(8):
-        found, corners = calib.can_calib(f'cali_0{i}.jpg')
-        if found:
-            print("Calibration pattern found.")
+    calib.save_normalized('./example/1103/normalized_cali_04.npy')
+    calib.convert2grayscale(340, 400)
+    found, corners = calib.can_calib(f'grayscale_data.jpg')
+    if found:
+        print("Calibration pattern found.")
--- compute_volume/tools/depthMap.py
+++ compute_volume/tools/depthMap.py
@@ -43,6 +43,6 @@ class depthMap:
         plt.close()
 
 if __name__ == "__main__":
-    example = depthMap('./example/normalized_cali_04.npy')
+    example = depthMap('./example/1103/calibration_03.npy')
     example.make_depth_Map()
     example.make_histogram()
